20baa8e8e52a1d2c9fbe240ddf58e54e82b8a6d3
Ellis Otoo <ellisotoo@gmail.com>
2021-07-14 20:08:16 +0000

    Images

--- src/catalogues/Groceries.js
+++ src/catalogues/Groceries.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Product from "../components/Product";
 import keysoap from "../img/products/keysoap.jpeg";
+// import milo from "../img/products/keysoap.jpeg";
+import milo from "../img/products/miloTin.jpeg";
+import sugar from "../img/products/sugar.jpeg";
+// import keysoap from "../img/products/keysoap.jpeg";
 
 function Groceries() {
   return (
@@ -13,11 +17,25 @@ function Groceries() {
       }}
     >
       <Product
-        name={`Perpy's Fresh Tomatoes`}
+        name={`Keysoap Bar`}
         description={"Sample Description"}
         featureList={["Feature One", "Feature Two"]}
         kiloPrice={2}
         imgsrc={keysoap}
+      />
+      <Product
+        name={`Milo Tin`}
+        description={"Sample Description"}
+        featureList={["Feature One", "Feature Two"]}
+        kiloPrice={22}
+        imgsrc={milo}
+      />
+      <Product
+        name={`Sugar`}
+        description={"Sample Description"}
+        featureList={["Feature One", "Feature Two"]}
+        kiloPrice={5}
+        imgsrc={sugar}
       />
     </div>
   );
